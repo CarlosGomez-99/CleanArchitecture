a02b3460a823e39b4ffbe90f218d7b3430fd3527
Carlos Gómez <drcgomez@hotmail.com>
2023-07-23 00:35:37 -0500

    Configuración EF y Dapper. Migraciones y configuracion de cadenas de conexion hacia la base de datos

---
+++ StreamCasa.Repository.EFCore/StreamCasaDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StreamCasa.Entities;
+
+namespace StreamCasa.Repository.EFCore
+{
+    public class StreamCasaDBContext : DbContext
+    {
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<Favorites> Favorites { get; set; }
+        public DbSet<Profiles> Profiles { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<UsersVideos> UsersVideos { get; set; }
+        public DbSet<Videos> Videos { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Data Source=PLSSYSTEM;Initial Catalog=StreamDB;User=sa;password=cgomez99;TrustServerCertificate=True");
+        //}
+    }
+}
