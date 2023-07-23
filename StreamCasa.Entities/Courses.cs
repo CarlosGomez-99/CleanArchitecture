201dfb6c8f6faaa1690d1eea315eab62eff7a9ca
Carlos Gómez <drcgomez@hotmail.com>
2023-07-22 23:51:56 -0500

    Archivos

---
+++ StreamCasa.Entities/Courses.cs
@@ -0,0 +1,17 @@
+﻿using StreamCasa.Abstractions.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamCasa.Entities
+{
+    public class Courses : IEntity
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Release { get; set; }
+        public List<Favorites> Favorites { get; set; }
+    }
+}
